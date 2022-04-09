69f0dbf19d4276fe2619d1a1a61ea561a18c0666
rmagley <rachelmagley@gmail.com>
2022-04-09 13:39:05 -0700

    Add About us photo

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -4,7 +4,7 @@ import './AboutUs.css'
 function AboutUs(props) {
     return (
         <div className='aboutus__div'>
-            Hello From About Us
+            <h3 className='aboutus__h3'>Full-Stack Product Development and Staff Augmentation Services</h3>
         </div>
     );
 }
