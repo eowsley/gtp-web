e8157cfb2c80317244a4e71729b8e3d0240c4228
rmagley <rachelmagley@gmail.com>
2022-04-09 11:35:39 -0700

    Scaffold Nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,15 +5,14 @@ import logo from './assets/logo.png';
 
 function Header(props) {
     return (
-			<div className='header__div'>
+			<header className='header__div'>
 				<img className='header__logo__img' src={logo} alt="logo"/>
 				<h1 className='header__h1'>Global Technology Partner</h1>
 				<h2 className='header__h2'>
 					Making startup dreams come true by being your product development
 					partners
 				</h2>
-				<Nav />
-			</div>
+			</header>
 		);
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 function Nav(props) {
     return (
-        <div>
-            
-        </div>
+        <nav className='nav__div'>
+            <ul>
+                <li>HOME</li>
+                <li>WORKING WITH US</li>
+                <li>OUR WORK</li>
+                <li>CONTACT US</li>
+            </ul>
+        </nav>
     );
 }
 
