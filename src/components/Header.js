195fb8ee448983f0e66db0ec976eeafe5e46ecfd
rmagley <rachelmagley@gmail.com>
2022-04-09 11:19:40 -0700

    Style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import Nav from './Nav';
-import './Header.css'
+import './Header.css';
+import logo from './assets/logo.png';
 
 function Header(props) {
     return (
-        <div className='header__div'>
-            <h1 className='header__h1'>Global Technology Partner</h1>
-            <h2 className='header__h2'>Making startup dreams come true by being your product development partners</h2>
-            <Nav/>
-        </div>
-    );
+			<div className='header__div'>
+				<img className='header__logo__img' src={logo} alt="logo"/>
+				<h1 className='header__h1'>Global Technology Partner</h1>
+				<h2 className='header__h2'>
+					Making startup dreams come true by being your product development
+					partners
+				</h2>
+				<Nav />
+			</div>
+		);
 }
 
 export default Header;
