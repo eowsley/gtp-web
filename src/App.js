f96a1831629c1ff0e62c698b0523ee23c74fc6a5
rmagley <rachelmagley@gmail.com>
2022-04-09 11:40:55 -0700

    Add routes on App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Nav from './components/Nav';
+import WorkingWithUs from './components/WorkingWithUs'
+import WorkingWithUsBanner from './components/WorkingWithUsBanner'
+import {Routes, Route, Link} from 'react-router-dom'
 
 function App() {
   return (
     <div>
       <Header/>
       <Nav/>
+      <WorkingWithUsBanner/>
+      <Footer/>
+      <Routes>
+        <Route path='/about' element={<WorkingWithUs/>}></Route>
+      </Routes>
     </div>
   );
 }
