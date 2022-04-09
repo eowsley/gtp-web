a0847b5b707008fd83644c7629c67f0009c475b4
rmagley <rachelmagley@gmail.com>
2022-04-09 10:43:20 -0700

    Add Header text and styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Nav from './Nav';
+import './Header.css'
 
 function Header(props) {
     return (
-        <div>
-            <h1>Global Technology Partner</h1>
+        <div className='header__div'>
+            <h1 className='header__h1'>Global Technology Partner</h1>
+            <h2 className='header__h2'>Making startup dreams come true by being your product development partners</h2>
             <Nav/>
         </div>
     );
