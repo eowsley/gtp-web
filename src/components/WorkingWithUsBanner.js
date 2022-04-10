4ec439dcd4ea01d3b20a4973aa5d4eefee133a94
rmagley <rachelmagley@gmail.com>
2022-04-09 23:44:06 -0700

    Complete Working With Us Banner

--- src/components/WorkingWithUsBanner.js
+++ src/components/WorkingWithUsBanner.js
@@ -6,7 +6,7 @@ import './WorkingWithUsBanner.css'
 function WorkingWithUsBanner(props) {
     return (
         <div className='workingwithusbanner__div'>
-            <Link to={<WorkingWithUs/>}>Working With Us</Link>
+            <Link to={<WorkingWithUs/>}><p>Working With Us</p></Link>
         </div>
     );
 }
