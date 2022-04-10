6edfc4bc1a09cad5814f9efee4c9391df0c7a2d9
rmagley <rachelmagley@gmail.com>
2022-04-10 11:42:02 -0700

    Add state to NAV for coditional styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,34 @@
+import { blue } from '@mui/material/colors';
 import React from 'react';
+import {useState} from 'react';
 import {Link} from 'react-router-dom';
 import './Nav.css';
 
 
 function Nav(props) {
+
+	const [link, setLink] = useState();
+
+	function handleChange(event) {
+		const newState = event.target.id;
+		console.log(newState);
+		setLink(newState);
+		console.log(link)
+	}
+
     return (
 			<nav>
 				<Link to='/'>
-					<button className='nav__btn'>HOME</button>
+					<button className='nav__btn' id="home" onClick={handleChange}>HOME</button>
 				</Link>
 				<Link to='/aboutus'>
-					<button className='nav__btn'>WORKING WITH US</button>
+					<button className='nav__btn' id='about'>WORKING WITH US</button>
 				</Link>
 				<Link to='/ourwork'>
-					<button className='nav__btn'>OUR WORK</button>
+					<button className='nav__btn' id='ourwork'>OUR WORK</button>
 				</Link>
 				<Link to='/contactus'>
-					<button className='nav__btn'>CONTACT US</button>
+					<button className='nav__btn' id='contact'>CONTACT US</button>
 				</Link>
 			</nav>
 		);
