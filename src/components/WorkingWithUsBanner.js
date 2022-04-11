16afbeca2e21c9db465ca210a7da1d7703e1147f
rmagley <rachelmagley@gmail.com>
2022-04-10 18:40:22 -0700

    Update banner styling

--- src/components/WorkingWithUsBanner.js
+++ src/components/WorkingWithUsBanner.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
-import WorkingWithUs from './WorkingWithUs';
 import './WorkingWithUsBanner.css'
 
 function WorkingWithUsBanner(props) {
