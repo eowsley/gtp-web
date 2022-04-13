44b3ce0050f288ba07fbb3c410c3c33c20dc481c
rmagley <rachelmagley@gmail.com>
2022-04-13 14:19:47 -0700

    Complete contact us component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,45 @@
 import React from 'react';
+import './Contact.css'
 
 function Contact(props) {
+
+
     return (
-        <div>
-            Hello from Contact US
-        </div>
-    );
+			<div className='contact__div'>
+				<div className='contact__heroimg'></div>
+				<h2 className='contact__h2'>Contact Us</h2>
+
+				<form>
+					<label htmlFor='name'>
+						<span className='contact__span'>Name </span> (required)
+					</label>
+					<br />
+					<input type='text' id='name' required={true} />
+
+					<br />
+					<label htmlFor='email'>
+						<span className='contact__span'>Email </span> (required)
+					</label>
+					<br />
+					<input type='email' id='email' required={true} />
+
+					<br />
+					<label htmlFor='comment'>
+						<span className='contact__span'>Comment </span> (required)
+					</label>
+					<br />
+					<input
+						className='contact__inputcomment'
+						type='text'
+						id='comment'
+						required={true}
+					/>
+                    <br/>
+                    <button className='contact__btn'>Submit</button>
+
+				</form>
+			</div>
+		);
 }
 
 export default Contact;
