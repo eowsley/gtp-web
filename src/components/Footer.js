e1bfd47c5b831a623751320ce70862c6522d324a
ramblelob <aileen.ramelb@gmail.com>
2022-04-13 19:50:40 -0700

    Update footer with Font Awesome icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,51 @@
 import React from 'react';
 import './Footer.css';
+import { FaMapMarkerAlt, FaMobileAlt, FaEnvelope } from 'react-icons/fa';
+
 
 function Footer(props) {
+
+
+
     return (
         <div className='footer__div' id="footer">
-            Hello from footer
+            
+                <ul className='footer_info' id="info">
+                    <li>
+                    <FaMapMarkerAlt />
+                        {/* <i className='fa-solid fa-location-dot'></i> */}
+                        274 Redwood Shores Parkway #504, Redwood City, CA 94065
+                    </li>
+                    <li>
+                        <FaMobileAlt />
+                        {/* <i className='fa-solid fa-mobile-screen-button'></i> */}
+                        650-339-3768
+                    </li>
+                    <li>
+                    <FaEnvelope />
+                        llin@globalpartner.com
+                    </li>
+                </ul>
+
         </div>
+     
     );
 }
 
 export default Footer;
+
+// import React from 'react';
+// import './Footer.css';
+// import { FaMapMarkerAlt, FaMobileAlt, FaEnvelope } from 'react-icons/fa';
+
+// function Footer(props) {
+//     return (
+//         <div className='footer__div' id="footer">
+//             <FaMapMarkerAlt /> address
+//             <FaMobileAlt /> phone
+//             <FaEnvelope />
+//         </div>
+//     );
+// }
+
+// export default Footer;
