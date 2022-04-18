5462af184cbdbdff5dd133e024cd575a462f9e38
rmagley <rachelmagley@gmail.com>
2022-04-18 12:02:49 -0700

    Scaffold Our Work

--- src/components/OurWork.js
+++ src/components/OurWork.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import './OurWork.css'
 
 function OurWork(props) {
     return (
-        <div>
-            Hello from our work
-        </div>
-    );
+			<div className='ourwork__div'>
+                <div className='ourwork__heroimg'></div>
+				<h2 className='ourwork__h2'>Our Work</h2>
+				<div className='ourwork__p'>Project Examples</div>
+				<div className='ourwork__ex1'>Social Communications Platform</div>
+				<div className='ourwork__ex2'>Mentor Marketplace</div>
+				<div className='ourwork__ex3'>Parking On Demand</div>
+				<div className='ourwork__ex4'>Food Catering Platform</div>
+				<div className='ourwork__ex5'>Language Learning</div>
+			</div>
+		);
 }
 
 export default OurWork;
